8453dde9e78199283802e3084dab45df112c2331
Taku <zhuozhuozhuol@gmail.com>
2015-03-27 18:17:24 -0400

    Create testOnClientCertificateAuthentication.sh

---
+++ testOnClientCertificateAuthentication.sh
@@ -0,0 +1,7 @@
+openssl s_client -connect zhubuntu14.spidynamics.com:443 -cert client.crt -key client.key
+#GET / HTTP/1.1
+#Host: foo(could be whatever)
+
+openssl s_client -connect zhubuntu14.spidynamics.com:80 
+#/ HTTP/1.1
+#Host: foo(could be whatever)
